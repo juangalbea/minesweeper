dcc9edb3198c09b6c22f4faf3aaeb26bae90e49d
Juan <juangalbea@gmail.com>
2018-10-07 18:14:38 +0100

    Loops

--- minesweeper.js
+++ minesweeper.js
@@ -1,6 +1,13 @@
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+  const board = [];
+  for (let i = 0; i < numberOfRows; i++) {
+    let row = [];
+    for (let j = 0; j < numberOfColumns; j++) {
+      row.push(' ');
+    };
+    board.push(row);
+  };
+  return board
+};
 
-let board = [
-  [' ', ' ', ' '],
-  [' ', ' ', ' '],
-  [' ', ' ', ' ']
-];
+console.log(generatePlayerBoard(2, 3));
