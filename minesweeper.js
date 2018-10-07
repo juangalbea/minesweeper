d04588276b3aff232e2c30514aee099b59bdb718
Juan <juangalbea@gmail.com>
2018-10-07 23:40:09 +0100

    generate boards

--- minesweeper.js
+++ minesweeper.js
@@ -10,4 +10,33 @@ const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
   return board
 };
 
-console.log(generatePlayerBoard(2, 3));
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  const board = [];
+  for (let i = 0; i < numberOfRows; i++) {
+    let row = [];
+    for (let j = 0; j < numberOfColumns; j++) {
+      row.push(null);
+    };
+    board.push(row);
+  };
+  let numberOfBombsPlaced = 0;
+  while (numberOfBombsPlaced < numberOfBombs) {
+  let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+  let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+  board[randomRowIndex][randomColumnIndex] = 'B';
+  numberOfBombsPlaced ++;
+  };
+  return board
+};
+
+const printBoard = board => {
+  console.log(board.map(row => row.join(' | ')).join('\n'));
+};
+
+let playerBoard = generatePlayerBoard(3, 4);
+let bombBoard = generateBombBoard(3, 4, 5);
+
+console.log('Player Board: ');
+printBoard(playerBoard);
+console.log('Bomb Board: ');
+printBoard(bombBoard);
