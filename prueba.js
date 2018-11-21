878d2c951bc8b784b446ba91f5abbb349b645b4f
Juan <juangalbea@gmail.com>
2018-11-21 23:34:24 +0000

    Classes

--- prueba.js
+++ prueba.js
@@ -1,14 +1,17 @@
-function isThisMonday(day) {
-  switch(day) {
-    case 1:
-      return true;
-      break;
-    case "Monday":
-      return true;
-      break;
-    default:
-      return false;
-  }
-}
+function run(N, M) {
+	let output = [];
+	for (i = N; i <= M; i++) {
+		if(i % 3 === 0 && i % 5 === 0) {
+			output[i - N] = 'FizzBuzz';
+		} else if (i % 3 === 0) {
+			output[i - N] = 'Fizz';
+		} else if (i % 5 === 0) {
+			output[i - N] = 'Buzz';
+		} else {
+			output[i - N] = i;
+		}
+	}
 
-console.log(isThisMonday('Monday'));
+	sequence = null;
+	return output;
+}
